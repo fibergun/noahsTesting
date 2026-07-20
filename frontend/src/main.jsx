6c930540d046828c8f18871c2dc41efb76e88208
fibergun <noahvandervuurst@hotmail.com>
2026-07-20 21:45:57 +0200

    setting up sessionsdata

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import {SessionProvider} from "./pages/login.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+
   <React.StrictMode>
-    <App />
+      <SessionProvider>
+          <App />
+      </SessionProvider>
   </React.StrictMode>,
 )
